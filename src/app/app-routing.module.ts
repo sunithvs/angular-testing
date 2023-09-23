6aac346d0ad130ac424426bf64479be6d1c4b96b
sunithvs <sunithvs2002@gmail.com>
2023-09-23 19:23:48 +0530

    all works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthComponent} from "./auth/auth.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {SignupComponent} from "./auth/signup/signup.component";
+import {HomeComponent} from "./home/home.component";
+import {MovieComponent} from "./movie/movie.component";
 
 
 const routes: Routes = [
@@ -8,6 +12,22 @@ const routes: Routes = [
     path: 'auth',
     component: AuthComponent,
   },
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'auth/signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'movie',
+    component:MovieComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  }
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
@@ -7,6 +8,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthRoutingModule } from './auth/auth-routing.module';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
 
 @NgModule({
@@ -14,12 +17,16 @@ import { AuthRoutingModule } from './auth/auth-routing.module';
     AppComponent,
     AuthComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthRoutingModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
